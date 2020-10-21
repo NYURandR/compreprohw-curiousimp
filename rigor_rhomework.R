3a1a2ad81b9243c7e6d8643762076926f3a0f6d0
curiousimp <ahachisu@g.ucla.edu>
2020-10-21 12:34:24 -0400

    Add files via upload

---
+++ rigor_rhomework.R
@@ -0,0 +1,19 @@
+#Go to https://classroom.github.com/a/cYkF9a_M to accept the homework, then:
+  
+#1) Create an R Project to pull “CompRepro.rmd” file from GitHub.
+
+#2) In the file, please describe one of the ways that Wilson et. al. suggest is 
+  #a good practice for computational reproducibility and how a tool like R can be used. 
+  #Write your answer below.
+
+#One good practice is tracking changes and recording how your projects evolve over time.
+#R is useful because it allows you to do this with commenting.
+#R is overall useful for faster statstical analysis and keeping your data in one place that can be easily looked up later.
+
+
+#3) Add a code chunk and find the square root of 4 to the 13th power. If you are unsure of what 
+  #command to use, google it. 
+
+(sqrt(4))^13
+
+#4) Save the file, commit the changes, and push the homework to the GitHub repository for the homework.
